7edb4d3b9b31da6b47e9a84265ed6aa1a547bb9d
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-05 17:55:25 -0300

    feat: create token validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -35,7 +35,20 @@ const isValidPassword = (req, res, next) => {
   next();
 };
   
+const isValidToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido!' });
+  }
+
+  next();
+};
+
 module.exports = {
   isValidEmail,
   isValidPassword,
+  isValidToken,
 };
