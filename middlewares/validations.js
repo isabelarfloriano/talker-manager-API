b2743da8f49cd9e651a651e0466a5987ff111d5e
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-05 12:33:41 -0300

    fix: dissolve change send to json on resolves

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -5,12 +5,12 @@ const isValidEmail = (req, res, next) => {
   const { email } = req.body;
 
   if (!email) { 
-    return res.status(HTTP_ERROR_STATUS).send({ 
+    return res.status(HTTP_ERROR_STATUS).json({ 
       message: 'O campo "email" é obrigatório' });
   }
 
   if (!emailRegex.test(email)) {
-    return res.status(HTTP_ERROR_STATUS).send({ 
+    return res.status(HTTP_ERROR_STATUS).json({ 
       message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
@@ -21,12 +21,12 @@ const isValidPassword = (req, res, next) => {
   const { password } = req.body;
   
   if (!password) { 
-    return res.status(HTTP_ERROR_STATUS).send({ 
+    return res.status(HTTP_ERROR_STATUS).json({ 
       message: 'O campo "password" é obrigatório' });
   }
 
   if (password.length >= 6) {
-    return res.status(HTTP_ERROR_STATUS).send({ 
+    return res.status(HTTP_ERROR_STATUS).json({ 
       message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
