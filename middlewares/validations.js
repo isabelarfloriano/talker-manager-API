0ce1baba86f91a0406d4e4092d7c734e4a8a3a85
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-05 19:43:29 -0300

    fix: lint token validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -41,7 +41,7 @@ const isValidToken = (req, res, next) => {
   if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
 
   if (authorization.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido!' });
+    return res.status(401).json({ message: 'Token inválido' });
   }
 
   next();
