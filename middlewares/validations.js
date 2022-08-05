ef22d69bf27efbd8020b3151d4e084e7582f7c1c
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-05 18:04:33 -0300

    feat: create name validation

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -47,8 +47,27 @@ const isValidToken = (req, res, next) => {
   next();
 };
 
+const isNameValid = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(HTTP_ERROR_STATUS).json({
+      message: 'O campo "name" é obrigatório',
+    }); 
+  }
+
+  if (name.length < 3) {
+    return res.status(HTTP_ERROR_STATUS).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    }); 
+  }
+
+  next();
+};
+
 module.exports = {
   isValidEmail,
   isValidPassword,
   isValidToken,
+  isNameValid,
 };
