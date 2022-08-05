fd659c6100e90fe4b5e6930e0fd4d9168016cc84
Isabela Rozani Floriano <irf161295@gmail.com>
2022-08-05 18:35:05 -0300

    feat: create watchedAt and rate validations

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -96,6 +96,33 @@ const isTalkValid = (req, res, next) => {
   next();
 };
 
+const isWatchedAtValid = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt } = talk;
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i; // Based on Bonus Activity - 22.5 - Course - Trybe
+
+    if (!dateRegex.test(watchedAt)) {
+    return res.status(HTTP_ERROR_STATUS).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    }); 
+  }
+
+  next();
+};
+
+const isRateValid = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate } = talk;
+
+    if (rate < 1 || rate > 5) {
+    return res.status(HTTP_ERROR_STATUS).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    }); 
+  }
+
+  next();
+};
+
 module.exports = {
   isValidEmail,
   isValidPassword,
@@ -103,4 +130,6 @@ module.exports = {
   isNameValid,
   isAgeValid,
   isTalkValid,
+  isWatchedAtValid,
+  isRateValid,
 };
